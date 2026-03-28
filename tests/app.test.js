b504a540b30414c22fea34c05833ce2b98dc33b5
ayham-kattan <ayhamkattan1@gmail.com>
2026-03-28 16:05:43 -0700

    test202

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const app = require('../app');
 
-describe('Express API Tests', () => {
+describe('Express API Testsssss', () => {
 
   test('GET / should return Hello World', async () => {
     const res = await request(app).get('/');
