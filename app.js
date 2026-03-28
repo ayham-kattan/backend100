760d4c0198f8e16e1308a3597b24304071f1fef5
ayham-kattan <ayhamkattan1@gmail.com>
2026-03-28 14:16:01 -0700

    test github action

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  try {
+    res.status(200).send('Hello World');
+  } catch (error) {
+    console.error('Error processing GET request:', error);
+    return res.status(500).send('Internal Server Error');
+  }
+});
+
+app.post('/', (req, res) => {
+  try {
+    const { name } = req.body;
+    res.status(200).send('Hello ' + name);
+  }   catch (error) {
+    console.error('Error processing POST request:', error);
+    return res.status(500).send('Internal Server Error');
+  }
+});
+
+module.exports = app;
---
+++ tests/app.test.js
@@ -0,0 +1,21 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('Express API Tests', () => {
+
+  test('GET / should return Hello World', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toBe(200);
+    expect(res.text).toBe('Hello World');
+  });
+
+  test('POST / should return Hello + name', async () => {
+    const res = await request(app)
+      .post('/')
+      .send({ name: 'Ayham' });
+
+    expect(res.statusCode).toBe(200);
+    expect(res.text).toBe('Hello Ayham');
+  });
+
+});
